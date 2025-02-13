72d377535832c51a0d576cd31debd6b77c336e6b
Chris Avina <chrisavina@Mac.attlocal.net>
2025-02-13 09:44:30 -0800

    Refactored q_learning_table class

--- smart_zerg_agent.py
+++ smart_zerg_agent.py
@@ -12,7 +12,7 @@ from pysc2.lib import actions
 from pysc2.lib import features
 from logger_config import logger, rl_logger
 import game_stats_tracker as gs
-
+from q_learning_table import *
 # KILL_UNIT_REWARD = 0.2
 # KILL_BUILDING_REWARD = 0.5
 
@@ -222,43 +222,43 @@ class SmartZergAgent(base_agent.BaseAgent):
 
 
 
-class QLearningTable:
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
-        self.actions = actions
-        self.lr = learning_rate
-        self.gamma = reward_decay
-        self.epsilon = e_greedy
-        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+# class QLearningTable:
+#     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+#         self.actions = actions
+#         self.lr = learning_rate
+#         self.gamma = reward_decay
+#         self.epsilon = e_greedy
+#         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
 
 
-    def choose_action(self, observation):
-        self.check_state_exists(observation)
+#     def choose_action(self, observation):
+#         self.check_state_exists(observation)
 
-        if np.random.uniform() < self.epsilon:
-            state_action = self.q_table.loc[observation, :]
+#         if np.random.uniform() < self.epsilon:
+#             state_action = self.q_table.loc[observation, :]
 
-            state_action = state_action.reindex(np.random.permutation(state_action.index))
+#             state_action = state_action.reindex(np.random.permutation(state_action.index))
 
-            action = state_action.idxmax()
-        else:
-            action = np.random.choice(self.actions)
-        return action
+#             action = state_action.idxmax()
+#         else:
+#             action = np.random.choice(self.actions)
+#         return action
     
 
-    def learn(self, s, a, r, s_):
-        self.check_state_exists(s_)
-        self.check_state_exists(s)
+#     def learn(self, s, a, r, s_):
+#         self.check_state_exists(s_)
+#         self.check_state_exists(s)
 
-        q_predict = self.q_table.loc[s, a]
+#         q_predict = self.q_table.loc[s, a]
 
-        if s_ != 'terminal':
-            q_target = r + self.gamma * self.q_table.loc[s_, :].max()
-        else:
-            q_target = r
+#         if s_ != 'terminal':
+#             q_target = r + self.gamma * self.q_table.loc[s_, :].max()
+#         else:
+#             q_target = r
 
-        self.q_table.loc[s, a] += self.lr * (q_target-q_predict)
+#         self.q_table.loc[s, a] += self.lr * (q_target-q_predict)
 
-    def check_state_exists(self, state):
-        if state not in self.q_table.index:
-            new_row = pd.DataFrame([[0] * len(self.actions)], columns=self.q_table.columns, index=[state])
-            self.q_table = pd.concat([self.q_table, new_row])
+#     def check_state_exists(self, state):
+#         if state not in self.q_table.index:
+#             new_row = pd.DataFrame([[0] * len(self.actions)], columns=self.q_table.columns, index=[state])
+#             self.q_table = pd.concat([self.q_table, new_row])
--- view_sparse_data.py
+++ view_sparse_data.py
@@ -7,6 +7,6 @@ q_table = pd.read_pickle(DATA_FILE, compression='gzip')
 
 best_action_indices = q_table.idxmax(axis=1)  # Get column with the highest Q-value
 
-print(q_table[0][0])
+print(q_table[0][0].dtypes)
 # q_table.to_csv("q_table.csv", index=True)
 # print("Q-table saved as q_table.csv")
